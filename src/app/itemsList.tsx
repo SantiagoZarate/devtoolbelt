f5655c62a53617944d3dab35db9cc8a87f1e1b66
SantiagoZarate <santiago2001zarate@gmail.com>
2024-06-30 22:40:20 -0300

    feat: open shadcn sheet when clickin item

--- src/app/itemsList.tsx
+++ src/app/itemsList.tsx
@@ -1,7 +1,10 @@
+"use client"
+
 import { ArrowRightTopMicroIcon } from "@/components/icons/ArrowRightTopMicroIcon"
-import { Text } from "@/components/ui/Text"
+import { StarMicroIcon } from "@/components/icons/StarMicroIcon"
+import { SheetContent, SheetDescription, SheetHeader, SheetTitle, SheetTrigger, Sheet } from "@/components/ui/sheet"
 import { Item } from "@/types/item"
-import Link from "next/link"
+import { Text } from "@/components/ui/Text"
 
 interface Props {
   items: Item[]
@@ -12,22 +15,43 @@ export function ItemsList({ items }: Props) {
     <ul className="w-full grid grid-cols-responsive gap-6 p-6">
       {
         items.map((item, index) => (
-          <Link
-            key={index}
-            href={"https://youtube.com"}
-            className="group aspect-[3/2] grid grid-rows-5 hover:shadow-md transition-shadow duration-300 shadow-sm rounded-xl border border-border">
-            <div className="row-span-3 border-b border-border"></div>
-            <footer className="row-span-2 p-4 flex flex-col gap-2">
-              <Text>{item.name}</Text>
-              <Text intent={"regular"}>{item.description}</Text>
-              <div className="group flex items-center gap-1">
-                <Text size={"small"} hoverable>Go to page</Text>
-                <span className="group-hover:-translate-y-[2px] group-hover:translate-x-[2px] transition duration-150 ">
-                  <ArrowRightTopMicroIcon />
-                </span>
-              </div>
-            </footer>
-          </Link>
+          <Sheet key={index}>
+            <article className=" hover:shadow-md transition-shadow duration-300 shadow-sm rounded-xl overflow-hidden border border-border">
+              <SheetTrigger className="w-full" >
+                <div className="w-full grid grid-rows-5">
+                  <div className="row-span-3 border-b border-border"></div>
+                  <section className="row-span-2 p-4 items-start flex flex-col gap-2">
+                    <Text>{item.name}</Text>
+                    <Text intent={"regular"}>{item.description}</Text>
+                  </section>
+                </div>
+              </SheetTrigger>
+              <footer className="flex border-t divide-x border-border">
+                <a href={"https://youtube.com"} className="group flex-1 justify-center group flex items-center gap-1 px-4 py-4 hover:bg-secondary transition">
+                  <Text size={"small"} hoverable>Go to page</Text>
+                  <span className="group-hover:-translate-y-[2px] group-hover:translate-x-[2px] group-hover:opacity-100 opacity-50 transition duration-150 ">
+                    <ArrowRightTopMicroIcon />
+                  </span>
+                </a>
+                <div className="group flex-1 justify-center group flex items-center gap-1 px-4 py-4 hover:bg-secondary transition">
+                  <Text size={"small"} hoverable>Add to favorites</Text>
+                  <span className="group-hover:opacity-100 opacity-50 transition">
+                    <StarMicroIcon />
+                  </span>
+                </div>
+              </footer>
+            </article>
+            <SheetContent>
+              <SheetHeader>
+                <SheetTitle>Are you absolutely sure?</SheetTitle>
+                <SheetDescription>
+                  This action cannot be undone. This will permanently delete your account
+                  and remove your data from our servers.
+                </SheetDescription>
+              </SheetHeader>
+            </SheetContent>
+          </Sheet>
+
         ))
       }
     </ul>
